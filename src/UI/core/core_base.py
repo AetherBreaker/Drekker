9addc576a86d6c5f86115599061cf39fc6eb7339
Jacob Ogden <jacob.ogden@yahoo.com>
2026-04-13 22:35:57 -0400

    WIP on the Attributes Tab

--- src/UI/core/core_base.py
+++ src/UI/core/core_base.py
@@ -1,6 +1,4 @@
-from types import TracebackType
-
-from textual.containers import ScrollableContainer
+from textual.binding import Binding
 from textual.content import ContentType
 from textual.widget import Widget
 from textual.widgets import TabPane
@@ -12,6 +10,21 @@ class CoreTabContainerBase(TabPane):
   sheet. For now, it's just a placeholder, but it allows us to easily add common features to
   all tabs in the future."""
 
+  BORDER_TITLE = "CoreTabContainerBase"
+
+  BINDINGS = [
+    Binding("up", "scroll_up", "Scroll Up", show=False),
+    Binding("down", "scroll_down", "Scroll Down", show=False),
+    Binding("left", "scroll_left", "Scroll Left", show=False),
+    Binding("right", "scroll_right", "Scroll Right", show=False),
+    Binding("home", "scroll_home", "Scroll Home", show=False),
+    Binding("end", "scroll_end", "Scroll End", show=False),
+    Binding("pageup", "page_up", "Page Up", show=False),
+    Binding("pagedown", "page_down", "Page Down", show=False),
+    Binding("ctrl+pageup", "page_left", "Page Left", show=False),
+    Binding("ctrl+pagedown", "page_right", "Page Right", show=False),
+  ]
+
   def __init__(
     self,
     title: ContentType,
@@ -21,11 +34,6 @@ class CoreTabContainerBase(TabPane):
     classes: str | None = None,
     disabled: bool = False,
   ):
-    self._container = ScrollableContainer(
-      *children,
-      name=f"{name}-container" if name else None,
-      id=f"{id}-container" if id else None,
-      classes=classes,
-      disabled=disabled,
+    super().__init__(
+      title, *children, name=name, id=id, classes=classes, disabled=disabled
     )
-    super().__init__(title, name=name, id=id, classes=classes, disabled=disabled)
--- src/UI/core/tab_attributes.py
+++ src/UI/core/tab_attributes.py
@@ -1,15 +1,19 @@
 """Attributes tab for character sheet."""
 
+from re import sub
+
 from textual.app import ComposeResult
-from textual.containers import ScrollableContainer
+from textual.containers import Container, Grid, HorizontalGroup, Middle, VerticalGroup
 from textual.widget import Widget
-from textual.widgets import Static
+from textual.widgets import Button, Label
 
 from UI.core.core_base import CoreTabContainerBase
 
 
 class AttributesTab(CoreTabContainerBase):
   """Attributes, Qualities, and derived statistics tab."""
+
+  BORDER_TITLE = "AttributesTab"
 
   def __init__(
     self,
@@ -25,5 +29,96 @@ class AttributesTab(CoreTabContainerBase):
     )
 
   def compose(self) -> ComposeResult:
-    with ScrollableContainer():
-      yield Static("[Attributes — coming soon]")
+    with VerticalGroup(classes="test vert1") as vert1:
+      vert1.border_title = "vert1"
+      with HorizontalGroup(classes="test horz2") as horz2:
+        horz2.border_title = "horz2"
+        with Container(
+          id="basic-data-container", classes="test placeholder"
+        ) as placeholder:
+          placeholder.border_title = "placeholder"
+        with Container(
+          id="appearance-container", classes="test placeholder"
+        ) as placeholder:
+          placeholder.border_title = "placeholder"
+      with HorizontalGroup(classes="test horz2") as horz2:
+        horz2.border_title = "horz2"
+        with VerticalGroup(classes="test vert3") as vert3:
+          vert3.border_title = "vert3"
+          yield AttrTable(
+            "Body",
+            "Agility",
+            "Reaction",
+            "Strength",
+            "Willpower",
+            "Logic",
+            "Intuition",
+            "Charisma",
+            "Edge",
+            "Magic",
+            "Resonance",
+          )
+        with VerticalGroup(classes="test vert3") as vert3:
+          vert3.border_title = "vert3"
+          with Container(
+            id="qualities-container", classes="test placeholder"
+          ) as placeholder:
+            placeholder.border_title = "placeholder"
+        with VerticalGroup(classes="test vert3") as vert3:
+          vert3.border_title = "vert3"
+          with Container(
+            id="quality-paths-container", classes="test placeholder"
+          ) as placeholder:
+            placeholder.border_title = "placeholder"
+          with Container(
+            id="critter-powers-container", classes="test placeholder"
+          ) as placeholder:
+            placeholder.border_title = "placeholder"
+
+
+class AttrTable(Grid):
+  """Table for displaying attributes and their values."""
+
+  BORDER_TITLE = "AttrTable"
+
+  def __init__(
+    self,
+    *attributes: str,
+    name: str | None = None,
+    id: str | None = None,
+    classes: str | None = None,
+    disabled: bool = False,
+  ):
+    super().__init__(name=name, id=id, classes=classes, disabled=disabled)
+
+    self.attributes = [
+      {
+        "display_name": attr.title(),
+        "id": sub(" +", "-", attr.lower()),
+        "value": 0,
+      }
+      for attr in attributes
+    ]
+    # self.styles.grid_size_rows = len(self.attributes)
+    # self.styles.grid_size_columns = 4
+
+  def compose(self) -> ComposeResult:
+    for attr in self.attributes:
+      with Middle():
+        yield Label(
+          attr["display_name"],
+          expand=False,
+          shrink=False,
+          id=f"{attr['id']}-label",
+          classes="attr-label attr-row",
+        )
+      yield Button(
+        "+", id=f"{attr['id']}-increment", classes="attr-inc attr-row attr-button"
+      )
+      with Middle():
+        yield Label(
+          f"{attr['value']}", id=f"{attr['id']}-value", classes="attr-value attr-row"
+        )
+      yield Button(
+        "-", id=f"{attr['id']}-decrement", classes="attr-dec attr-row attr-button"
+      )
