f4f0326198bc3b5a7d0863e1e811fe836f7e619b
Jacob Ogden <jacob.ogden@yahoo.com>
2026-06-07 19:51:31 -0400

    Added a uv script entrypoint for generating datapack schema file

--- src/drekker/datapacks/models/serializer.py
+++ src/drekker/datapacks/models/serializer.py
@@ -1,7 +1,10 @@
 # Standard library imports
+from json import dump
+from pathlib import Path
 from typing import Annotated
 
 # Third party imports
+import typer
 from pydantic import BaseModel, Field
 
 # First party imports
@@ -13,3 +16,17 @@ class DatapackSerializer(BaseModel):
   model_config = PYDANTIC_CONFIG
 
   qualities: Annotated[list[Quality] | None, Field()] = None
+
+
+DEFAULT_SCHEMA_OUTPUT_PATH = Path.cwd() / "datapack_schema.json"
+
+
+def _generate_schemas(output_path: Path = DEFAULT_SCHEMA_OUTPUT_PATH) -> None:
+  if not output_path.parent.exists():
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+  with output_path.open("w") as f:
+    dump(DatapackSerializer.model_json_schema(), f, indent=2)
+
+
+def generate_schemas() -> None:
+  typer.run(_generate_schemas)
