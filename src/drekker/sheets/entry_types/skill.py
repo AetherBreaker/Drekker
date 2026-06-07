7559b95da692bd6262593f2541c67e5fdf533436
Jacob Ogden <jacob.ogden@yahoo.com>
2026-06-07 19:51:00 -0400

    Fixed some module imports

--- src/drekker/sheets/entry_types/skill.py
+++ src/drekker/sheets/entry_types/skill.py
@@ -1,10 +1,10 @@
 # Standard library imports
 from typing import Literal
-from typing.enums import SkillName
 
 # First party imports
+from drekker.sheets.entry_types import EntryType
 from drekker.sheets.entry_types.attrbase import AttrEntryBase
-from sheets.entry_types import EntryType
+from drekker.typing.enums import SkillName
 
 
 class SkillEntry(AttrEntryBase):
